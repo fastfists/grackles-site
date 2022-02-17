4c07f1e9ba0f85484e47a9db9097969a4927fd36
Denzell Ford <fdenzell@gmail.com>
2022-02-17 12:32:54 -0600

    typo in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
-          error: colors.deeporange.accent4,
+          error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
       }
